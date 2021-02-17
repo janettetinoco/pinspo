7adfadc587430402a2971f4634362bfe1b502fd0
Janette <tinoconjanette@gmail.com>
2021-02-16 20:41:54 -0800

    Fix Board edit button

--- frontend/components/boards/board_profile.jsx
+++ frontend/components/boards/board_profile.jsx
@@ -19,6 +19,10 @@ class BoardProfile extends React.Component{
                     <div className="board-profile-1">
                         <div >
                             <h1 className="board-profile-title">{board.title}</h1>
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                                width="48" height="48"
+                                viewBox="0 0 172 172"
+                                style={{fill:"#000000"}}><g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{mixBlendMode: "normal"}}><path d="M0,172v-172h172v172z" fill="none"></path><path d="M86,171.14c-47.02152,0 -85.14,-38.11848 -85.14,-85.14v0c0,-47.02152 38.11848,-85.14 85.14,-85.14h0c47.02152,0 85.14,38.11848 85.14,85.14v0c0,47.02152 -38.11848,85.14 -85.14,85.14z" fill="#cccccc"></path><g fill="#000000"><path d="M124.7,101.48c-8.53722,0 -15.48,-6.94278 -15.48,-15.48c0,-8.53722 6.94278,-15.48 15.48,-15.48c8.53722,0 15.48,6.94278 15.48,15.48c0,8.53722 -6.94278,15.48 -15.48,15.48zM47.3,101.48c-8.53722,0 -15.48,-6.94278 -15.48,-15.48c0,-8.53722 6.94278,-15.48 15.48,-15.48c8.53722,0 15.48,6.94278 15.48,15.48c0,8.53722 -6.94278,15.48 -15.48,15.48zM86,101.48c-8.53722,0 -15.48,-6.94278 -15.48,-15.48c0,-8.53722 6.94278,-15.48 15.48,-15.48c8.53722,0 15.48,6.94278 15.48,15.48c0,8.53722 -6.94278,15.48 -15.48,15.48z"></path></g></g></svg>
                         </div>
                         <div className="user-icon">
                             <h1 className="user-letter">{this.userInfo.username[0]}</h1>
