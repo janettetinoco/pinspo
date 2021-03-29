0506854b598dc11d5c27c6da5baf9ee15e58152d
janettetinoco <52670122+janettetinoco@users.noreply.github.com>
2021-03-28 23:17:39 -0700

    add about links

--- frontend/components/headers/logged_off_header.jsx
+++ frontend/components/headers/logged_off_header.jsx
@@ -12,6 +12,8 @@ class LoggedOffHeader extends React.Component{
                         <h3>Pinspo</h3>
                     </div>
                     <nav className="nav-buttons">
+                        <a href='https://github.com/janettetinoco'><img src="https://img.icons8.com/ios-filled/50/000000/github.png" /></a>
+                        <a href='https://www.linkedin.com/in/janette-tinoco-b15665192/'><img src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled--v1.png" /></a>
                         <button className='red-button' onClick={() => openModal('login')}>Login</button>
                         <button className='grey-button' onClick={() => openModal('signup')}>Signup</button>
                     </nav>
