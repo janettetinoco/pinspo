bcb7f51ebd1edd3a88b51af20fdd8625cd58e30b
janettetinoco <52670122+janettetinoco@users.noreply.github.com>
2021-03-28 22:40:01 -0700

    add splash animation

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -8,7 +8,7 @@ class SplashPage extends React.Component{
                     Get inspired for <br/> your next idea
                 </div>
                 <div className="splash-columns">
-                    <div className="splash-img" id="col-1"><img src={window.splash1URL} /> <img src={window.splash8URL} /></div>
+                    <div className="splash-img" id="col-1" ><img src={window.splash1URL} /> <img src={window.splash8URL} /></div>
                     <div className="splash-img" id="col-2"><img src={window.splash2URL} /> <img src={window.splash9URL} /></div>
                     <div className="splash-img" id="col-3"><img src={window.splash5URL} /> <img src={window.splash10URL} /></div>
                     <div className="splash-img" id="col-4"><img src={window.splash3URL} /> <img src={window.splash7URL} /></div>
